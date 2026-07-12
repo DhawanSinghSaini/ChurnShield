b09967ce8d23511cbf5c770146470c93d313044d
DhawanSinghSaini <161929806+DhawanSinghSaini@users.noreply.github.com>
2026-07-13 00:14:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'https://churnshield-beta.vercel.app' }));
 app.use(express.json());
 
 // Routes
